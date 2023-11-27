f88ec3f4ff46dd3104e405108564cdcbffa91a0a
Hugo Petla <hugopetla@gmail.com>
2023-11-26 22:35:26 -0300

    Created the todo insertion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import "./App.module.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import styles from "./App.module.css";
+import { CreateTodo } from "./components/CreateTodo";
 import { Header } from "./components/Header";
 
+export interface Task {
+  id: string;
+  title: string;
+  isComplete: boolean;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: uuidv4(), title: "Lavar a louça", isComplete: false },
+  ]);
+
+  function handleCreateNewTodo(newTask: Task) {
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <CreateTodo onHandleCreateNewTask={handleCreateNewTodo} />
+        {tasks.map((task) => (
+          <div key={task.id}>
+            <h1>{task.title}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,57 @@
+import { PlusCircle } from "@phosphor-icons/react";
+import { v4 as uuidv4 } from "uuid";
+
+import styles from "./CreateTodo.module.css";
+import { Task } from "../App";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
+
+interface CreateTodoProps {
+  onHandleCreateNewTask: (task: Task) => void;
+}
+
+export function CreateTodo({ onHandleCreateNewTask }: CreateTodoProps) {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  function handleNewTaskTitleChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("");
+    setTaskTitle(event.target.value);
+  }
+
+  function onPrepareCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    onHandleCreateNewTask({
+      id: uuidv4(),
+      title: taskTitle,
+      isComplete: false,
+    });
+
+    setTaskTitle("");
+  }
+
+  function handleNewTaskTitleInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório");
+  }
+
+  const isNewTaskTitleEmpty = taskTitle.length === 0;
+
+  return (
+    <form className={styles.wrapper} onSubmit={onPrepareCreateNewTask}>
+      <input
+        required
+        className={styles.input}
+        placeholder="Adicione uma nova tarefa"
+        value={taskTitle}
+        onChange={handleNewTaskTitleChange}
+        onInvalid={handleNewTaskTitleInvalid}
+      ></input>
+      <button
+        type="submit"
+        className={styles.button}
+        disabled={isNewTaskTitleEmpty}
+      >
+        Criar <PlusCircle />
+      </button>
+    </form>
+  );
+}
